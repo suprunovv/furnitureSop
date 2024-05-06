5e3de6de80b90cd1520ceb6bac1940c6ff09b17e
Suprunov Anton <suprunovvanton666@gmail.com>
2024-05-06 19:56:24 +0300

    Главный проект

---
+++ furnitureApp/furnitureApp/furnitureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  furnitureAppApp.swift
+//  furnitureApp
+//
+//  Created by MacBookPro on 06.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct furnitureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
